769c195565b33bcd58b36302f451b306144ee3cf
alelipona <vyacheslav.zubenko@gmail.com>
2024-03-04 00:39:18 +1100

    Initial starting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'AllCommands.TOP',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/alelipona/allcommands.top',
 			},
 			sidebar: [
 				{
